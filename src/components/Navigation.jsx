38db646ec0cada9b968b8566ef78823c1d0b2a32
Amol tukaram Kadam <amolkadam1274@gmail.com>
2025-12-23 17:43:07 +0530

    Change color of Navigation on dark

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <nav className="relative top-0 left-0 right-0 bg-white dark:bg-gray-900 shadow-lg z-50 transition-colors duration-300">
+    <nav className="relative top-0 left-0 right-0 bg-white dark:bg-gray-950 shadow-lg z-50 transition-colors duration-300">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-18 lg:h-18">
           {/* Logo */}
